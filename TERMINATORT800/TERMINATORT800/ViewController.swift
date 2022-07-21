b8d1f5751d3e056dae070ca3d7e232a88ba89679
Павел <terminator@MBP-Pavel.Dlink>
2022-07-21 18:26:56 +0300

    createdProject

---
+++ TERMINATORT800/TERMINATORT800/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TERMINATORT800
+//
+//  Created by Павел on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
